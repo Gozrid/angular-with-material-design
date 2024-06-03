67a32ef8220dab7f2e5e8c7e2ac1054fa04f90d4
Fabian Senftleben <fabiansenftlebe@gmail.com>
2024-06-03 11:14:30 +0200

    feat(INIT): Added files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,50 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {MatButtonToggle} from "@angular/material/button-toggle";
+import {MatFormField, MatLabel} from "@angular/material/form-field";
+import {MatInput} from "@angular/material/input";
+import {FormsModule} from "@angular/forms";
+import {NgClass, NgStyle} from "@angular/common";
+import {MatCard} from "@angular/material/card";
+import {MatButton} from "@angular/material/button";
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    MatSlideToggleModule,
+    MatButtonToggle,
+    MatFormField,
+    MatLabel,
+    MatInput,
+    FormsModule,
+    NgClass,
+    NgStyle,
+    MatCard,
+    MatButton
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-with-material-design';
+  textValue = "Hier eingeben!";
+  labelText = "Hier eingeben!";
+  autoUpdateEnabled = false;
+
+  manualUpdateText() {
+    this.labelText = this.textValue;
+  }
+
+  toggleAutoUpdate() {
+    this.autoUpdateEnabled = !this.autoUpdateEnabled;
+  }
+
+  autoUpdate() {
+    if (this.autoUpdateEnabled) {
+      this.labelText = this.textValue;
+    }
+  }
 }
